df992aa20b1409d0d49d55d24a93ce810388a0e2
Lokeshshah02 <lokeshkumarshah112@gmail.com>
2023-10-02 13:58:27 +0530

    demo changes

--- src/components/HomeComponents/RegisterPage.js
+++ src/components/HomeComponents/RegisterPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./registerpage.scss";
 import { useNavigate } from "react-router-dom";
 
-
+////hello
 const RegisterPage = () => {
   const navigate = useNavigate();
   
