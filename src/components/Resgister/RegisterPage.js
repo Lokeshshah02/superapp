505eb0c9a0c8f5d1ebdc4a2844b8db0fe8cf3f4d
Lokesh Shah <118357743+Lokeshshah02@users.noreply.github.com>
2023-11-26 08:56:15 +0530

    Update RegisterPage.js

--- src/components/Resgister/RegisterPage.js
+++ src/components/Resgister/RegisterPage.js
@@ -115,7 +115,7 @@ const phoneRegex = /^(?:\+1|1)?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})$
             {
               error && <p className="errorAlert"> {error.checkbox }</p>
             }
-            <button>SING UP</button>
+            <button>SIGN UP</button>
             <p>By clicking on Sign up. you agree to Superapp<span> Terms and Conditions of Use</span></p>
             <p>To learn more about how Superapp collects, uses, shares and protects your personal data please head Superapp<span>Privacy Policy</span></p>
 
